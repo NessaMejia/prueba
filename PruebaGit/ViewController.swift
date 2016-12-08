1926667151474a28fcbcdc4cd815704afb2f533c
Vanessa Mejia <vanessamejia@MacBook-Pro-de-Vanessa.local>
2016-12-08 14:03:52 -0600

    prueba de modificacion

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        //agregue otro comentario de prueba
         // Dispose of any resources that can be recreated.
     }
 
